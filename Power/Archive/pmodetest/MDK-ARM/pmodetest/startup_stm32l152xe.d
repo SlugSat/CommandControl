95a2eb544a993a138c7bb8467ab66a6398d1c827
Spencer <sneuschm@ucsc.edu>
2019-05-02 13:25:45 -0700

    Cleaned up the power modes directory

---
+++ Power/Archive/pmodetest/MDK-ARM/pmodetest/startup_stm32l152xe.d
@@ -0,0 +1,1 @@
+pmodetest\startup_stm32l152xe.o: startup_stm32l152xe.s
---
+++ Power/PowerModes_LogicAnal/pmode_test_rcv/MDK-ARM/pmode_test_rcv/transitions.d
@@ -0,0 +1,8 @@
+pmode_test_rcv\transitions.o: ..\Src\transitions.c
+pmode_test_rcv\transitions.o: ../Inc/PowerModes.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\stdlib.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\stdio.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\stdint.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\string.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\errno.h
+pmode_test_rcv\transitions.o: E:\Keil\Core\ARM\ARMCC\Bin\..\include\assert.h
