34df98ab912746357de6c81050fc123248e6647b
Olyad Mulisa Kebede <okebede@crm.ucsc.edu>
2019-05-08 14:44:02 -0700

    Spi_lib_tweak

--- Spi_FRAM/SPI_FRAM.h
+++ Spi_FRAM/SPI_FRAM.h
@@ -1,7 +1,7 @@
 //This is the library that lets you use the SPI FRAM.
 //please make sure that you have designated GPIO A9 as the chip select
 //you are only allowed to use macros for adresses in both functions
-
+#include "stm32l1xx_hal.h"
 //opcodes
 #define Write_Opcode        (2)
 #define Read_Opcode         (3)
@@ -9,19 +9,19 @@
 #define Wren_opcode         (6)
 
 //adresses
-#define latitude            (0)
-#define longitude           (4)
-#define altitude            (8)
-#define time                (100)
-#define battery_level       (200)
-#define PM_state            (300)
-#define I2c_datapoints      (400)
-#define Sun_State           (500)
-#define LT_status           (600)
-#define LT_Daily_users      (700)
-#define Power_status        (800) 
+#define SPI_latitude_addr            (0)
+#define SPI_longitude_addr           (4)
+#define SPI_altitude_addr            (8)
+#define SPI_time_addr                (100)
+#define SPI_battery_level_addr       (200)
+#define SPI_PM_state_addr            (300)
+#define SPI_I2c_datapoints_addr      (400)
+#define SPI_Sun_State_addr           (500)
+#define SPI_LT_status_addr           (600)
+#define SPI_LT_Daily_users_addr      (700)
+#define SPI_Power_status_addr        (800) 
 
 
-void SPI_FRAM_Read( uint16_t address, uint8_t *pRxData,uint8_t size);
+void SPI_FRAM_Read( SPI_HandleTypeDef hspi1,uint16_t address, uint8_t *pRxData,uint8_t size);
 
-void SPI_FRAM_Write( uint16_t address, uint8_t *pTxData,uint8_t size);
+void SPI_FRAM_Write( SPI_HandleTypeDef hspi1,uint16_t address, uint8_t *pTxData,uint8_t size);
