f42aab723d9d1801cdcb889b6690e3a7adbe0744
XentropyX <alec.martinez@gmail.com>
2019-05-12 14:17:25 -0700

    added spi fram test project

---
+++ Spi_FRAM/Spi_Fram_Test/Inc/SPI_FRAM.h
@@ -0,0 +1,37 @@
+#ifndef SPI_FRAM_LIB
+#define SPI_FRAM_LIB
+//This is the library that lets you use the SPI FRAM.
+//please make sure that you have designated GPIO A9 as the chip select
+//you are only allowed to use macros for adresses in both functions
+
+#include "main.h"
+
+// Constants
+#define WRITE_CMD_LEN	(4)
+#define READ_CMD_LEN	(3) 
+
+//opcodes
+#define WRITE_OP        (2)
+#define READ_OP         (3)
+#define WRDI_OP         (4)
+#define WREN_OP         (6)
+
+//adresses
+#define SPI_LAT_ADDR						(0)   // Latitude
+#define SPI_LONG_ADDR						(4)   // Longitude
+#define SPI_ALT_ADDR						(8)   // Altitude
+#define SPI_TIME_ADDR						(100) // Time 
+#define SPI_BATT_LEVEL_ADDR			(200) // Battery Level
+#define SPI_PM_STATE_ADDR				(300) // Power modes state
+#define SPI_LOG_DATA_TIME_ADDR	(400) // Log data at a certain time
+#define SPI_NUM_DATA_ADDR 			(500) 
+#define SPI_SOLAR_VECTOR_ADDR		(600)
+#define SPI_LT_STAT_ADDR				(700)
+#define SPI_LT_DAILY_USERS_ADDR (800)
+#define SPI_POWER_STAT_ADDR     (900) 
+#define SPI_MECH_STATE_ADDR			(1000)
+
+void SPI_FRAM_Read(SPI_HandleTypeDef *hspi, uint16_t address, uint8_t *pRxData,uint8_t size);
+
+void SPI_FRAM_Write(SPI_HandleTypeDef *hspi, uint16_t address, uint8_t *pTxData,uint8_t size);
+#endif // SPI_FRAM_LIB
